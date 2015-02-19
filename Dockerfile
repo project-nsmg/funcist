090e603ce3153364da8d6085e27629a0ee7e30ce
sorpa'as plat <me@sorpaas.com>
2015-02-19 17:54:21 +0800

    the first prototype of funcist

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,8 @@ MAINTAINER sorpa'as plat <me@sorpaas.com>
 RUN mkdir /src
 WORKDIR /src
 
+RUN npm install -g nodemon
 COPY . /src
 
 EXPOSE 8080
-CMD npm install && node index.js
+CMD npm install && nodemon index.js
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var request = require('request');
 
 // Constants
 var PORT = 8080;
@@ -6,7 +7,23 @@ var PORT = 8080;
 // App
 var app = express();
 app.get('/', function (req, res) {
-    res.send('Hello world\n');
+    res.send('A prototype of a micro-service hosting service. An example can be retrieved from https://funcist.ns.mg/7c9600b9d2a5f3b991ab\n');
+});
+
+app.get('/:gist_id', function (req, res) {
+    request({
+        url: 'https://api.github.com/gists/' + req.params.gist_id,
+        headers: {
+            'User-Agent': 'request'
+        }
+    }, function(error, response, body) {
+        var files = JSON.parse(body).files;
+        var filename = Object.keys(files)[0];
+        var content = files[filename].content;
+        var m = new module.constructor();
+        m._compile(content, filename);
+        res.send(m.exports);
+    });
 });
 
 app.listen(PORT);
